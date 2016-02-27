bc2a75ac147090f82dc062b1d5205d01d8fc846d
jasonshaffner <jason@shaffner.us>
2016-02-27 11:20:15 -0600

    fixed some concurrency issues

--- iTTY/format.py
+++ iTTY/format.py
@@ -7,53 +7,62 @@ class Format(object):
 		siftout = kwargs.get('siftout', [])
 		dontprint = ['enable', 'Password:', 'terminal length', 'screen-length', 'Screen length',\
 			'environment no more', '{master', 'Building config', 'Mon', 'Tue', 'Wed', \
-			'Thu', 'Fri', 'Sat', 'Sun', 'terminal pager'] + siftout
-		output = []
+			'Thu', 'Fri', 'Sat', 'Sun',] + siftout
+		temp = []
 		for entry in input:
 			for line in entry:
-				if not line.strip() or any(n in line for n in dontprint): continue
-				output.append(line)
-		return output
-
-	@staticmethod
-	def makeline(count, linechar="-"):
-		return linechar * count
+				if not line.strip(): continue
+				if any(n in line for n in dontprint): continue
+				temp.append(line)
+		return temp
 
 	@staticmethod
 	def underline(input, linechar="-"):
-		return input.strip() + '\n' + Format.makeline(len(input.strip()), linechar)
+		line = ""
+		for i in range(len(input.strip())):
+			line = line + linechar
+		return input.strip() + '\n' + line
 
 	@staticmethod
-	def padleft(input, count, padchar=" "):
-		return Format.makeline(count, padchar) + " " + input.strip()
-
-	@staticmethod
-	def padright(input, count, padchar=" "):
-		return input.strip() + " " + Format.makeline(count, padchar)
+	def makeline(count, linechar="-"):
+		line = ""
+		for i in range(count):
+			line = line + linechar
+		return line
 
 	@staticmethod
 	def pad(input, count, padchar=" "):
-		return Format.padleft(Format.padright(input.strip(), count, padchar), count, padchar)
+		input = " " + input.strip() + " "
+		for i in range(count):
+			input = padchar + input + padchar
+		return input
+
+	@staticmethod
+	def padleft(input, count, padchar=" "):
+		for i in range(count): input = padchar + input
+		return input
+
+	@staticmethod
+	def padright(input, count, padchar=" "):
+		for i in range(count): input += padchar
+		return input
 
 	@staticmethod
 	def columnize(input, bars=0, width=0):
 		maxlen = []
-		for line in input:
+		for i in input:
 			n = 0
-			for entry in line:
-				if len(maxlen) < len(line): maxlen.append(len(entry.strip()))
-				else: maxlen[n] = max(maxlen[n], len(entry.strip()))
+			for j in i:
+				if len(maxlen) < len(i): maxlen.append(len(j))
+				else: maxlen[n] = max(maxlen[n], len(j))	
 				n += 1
-		output = '\n'
-		for line in input:
+		for i in range(len(input)):
 			n = 0
-			if bars: tmp = '| '
-			else: tmp = ""
-			for entry in line:
-				padding = maxlen[n] - len(entry)
-				if bars: tmp += Format.padright(Format.padright(entry.strip(), padding + width/2) + '|', width/2)
-				else: tmp += Format.padright(entry.strip(), padding + width) 
+			for j in range(len(input[i])):
+				padding = maxlen[n] - len(input[i][j]) 
+				if bars: 
+					input[i][j] = Format.padright(Format.padright(input[i][j], padding + width/2) + '|', width/2)
+					if j == 0: input[i][j] = '| ' + input[i][j]
+				else: input[i][j] = Format.padright(input[i][j], padding + width) 
 				n += 1
-			if bars and (line == input[0] or line == input[-1]): tmp = Format.underline(tmp) 
-			output += tmp + '\n'
-		return output
+		return input
