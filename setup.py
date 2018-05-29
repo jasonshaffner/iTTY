eb08aacedd8e5ecfbfbd17f29f82d70a003878f2
Jason Shaffner <jason.shaffner@charter.com>
2018-05-29 10:31:32 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(name='iTTY',
 	version='0.2', 
 	description='Intelligent TTY Utilities for Network Administration',
 	author='Jason Shaffner, Patrick Lawless',
-	url='https://github.com/jasonshaffner/iTTY',
+	url='https://git.netops.charter.com/jasonshaffner/iTTY',
 	packages=['iTTY'], 
 	scripts=['scripts/intflaplist', 'scripts/ipspace', 'scripts/runcommands', \
 		'scripts/checklight', 'scripts/checkversion', 'scripts/checkowner',\
