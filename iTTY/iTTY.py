1c378cf1b49e4581a9562d3796619a23e8e7f6c0
jasonshaffner <jason.shaffner@charter.com>
2018-08-23 22:40:56 -0600

    further translation of the telnetlib functionality

--- iTTY/iTTY.py
+++ iTTY/iTTY.py
@@ -180,7 +180,7 @@ class iTTY:
     def rununseccommands(self, command_delay, commandheader=0, done=False):
         for command in self.commands:
             self.session.write((command.strip() + '\r').encode())
-            n, m, output = self.session.expect([re.compile(self.prompt), ], command_delay)
+            n, m, output = self.session.expect([re.compile(self.prompt.encode()), ], command_delay)
             time.sleep(command_delay)
             if commandheader:
                 self.addtooutput(['\n' + _underline(command), ])
