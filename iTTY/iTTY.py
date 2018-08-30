bb3eef816cd1f4d121aac3862b98d83dcfa8234b
jasonshaffner <jason.shaffner@charter.com>
2018-08-30 10:31:15 -0600

    adding docstrings and moving to 0.4

--- iTTY/iTTY.py
+++ iTTY/iTTY.py
@@ -1,9 +1,30 @@
-import getpass, telnetlib, time, asyncio, re, paramiko
+"""
+Telnet and SSH Client
+"""
+
+import getpass
+import telnetlib
+import time
+import asyncio
+import re
+import paramiko
+
 paramiko.util.log_to_file('/dev/null')
 
 class iTTY:
-    #Factory, optional keyword args: host, username, password
+    """
+    iTTY is a class to ensure easy logging in and running of commands on multiple platforms.
+    This is developed by network engineers specifically for logging in to network devices (routers, switches, firewalls, etc.)
+    but could be extended for other systems should the need arise. We build off the standard built-in telnetlib and paramiko so
+    we have multiple ways to log in. We first attempt secure (ssh) logins, then use telnet as a backup.
+    This has been tested to work with Cisco IOS-XR, Cisco IOS, Cisco ASA, Juniper JUNOS, and Alcatel/Nokia TiMOS.
+    """
+
+
     def __init__(self, **kwargs):
+        """
+        Factory, optional keyword args: host, username, password
+        """
         self.host = kwargs.get('host', None)
         self.username = kwargs.get('username', None)
         self.password = kwargs.get('password', None)
@@ -14,46 +35,81 @@ class iTTY:
         self.commands = []
         self.output = []
 
+
     def __enter__(self):
+        """
+        Support for "with" statements
+        """
         self.login()
 
+
     def __exit__(self, *args):
+        """
+        Support for "with" statements
+        """
         self.logout()
+
 
     async def __aenter__(self):
+        """
+        Support for "async with" statements
+        """
         await self.async_login()
 
+
     async def __aexit__(self, *args):
+        """
+        Support for "async with" statements
+        """
         self.logout()
 
-    #Sets which host to login and run commands
-    def sethost(self, host):
+
+    def set_host(self, host):
+        """
+        Sets which host to login and run_ commands
+        """
         self.host = host
 
-    #Returns host (if none set, default is None)
-    def gethost(self):
+
+    def get__host(self):
+        """
+        Returns host (if none set, default is None)
+        """
         return self.host
 
-    def clearhost(self):
+
+    def clear_host(self):
         self.host = None
 
-    #Sets username and password used for login
-    def setlogin(self, **kwargs):
-        self.username = kwargs.get('username', None)
-        if not self.username: self.username = input("Username: ")
-        self.password = kwargs.get('password', None)
-        if not self.password: self.password = getpass.getpass()
 
-    #Returns username (if none set, default is None)
-    def getusername(self):
+    def set_login(self, **kwargs):
+        """
+        Sets username and password used for login
+        """
+        self.username = kwargs.get('username', None)
+        if not self.username:
+            self.username = input("Username: ")
+        self.password = kwargs.get('password', None)
+        if not self.password:
+            self.password = getpass.getpass()
+
+
+    def get__username(self):
+        """
+        Returns username (if none set_, default is None)
+        """
         return self.username
 
-    def clearlogin(self):
+
+    def clear_login(self):
         self.username = None
         self.password = None
 
-    #Verifies that all necessary login parameters are set, returns 0 if one is missing
+
     def verifyloginparameters(self):
+        """
+        Verifies that all necessary login parameters are set_, returns 0 if one is missing
+        """
         flag = 1
         if not self.username:
             print("No username specified")
@@ -66,122 +122,176 @@ class iTTY:
             flag = 0
         return flag
 
-    #Takes prompt as arg, returns digit signifying type of OS
-    def setos(self, prompt):
-        if re.search('[A-B]:.*#', str(prompt)): self.os = 1 #ALU
-        elif re.search('CPU.*#', str(prompt)): self.os = 2  #XR
-        elif re.search('.*#', str(prompt)): self.os = 3     #IOS
-        elif re.search(self.username + '@.*>', str(prompt)): self.os = 4  #JUNOS
+
+    def set_os(self, prompt):
+        """
+        Takes prompt as arg, returns digit signifying type of OS
+        """
+        if re.search('[A-B]:.*#', str(prompt)):
+            self.os = 1 #ALU
+        elif re.search('CPU.*#', str(prompt)):
+            self.os = 2  #XR
+        elif re.search('.*#', str(prompt)):
+            self.os = 3    #IOS
+        elif re.search(self.username + '@.*>', str(prompt)):
+            self.os = 4  #JUNOS
         elif re.search('.*>', str(prompt)):
             self.os = 5  #ASA
-            if self.shell: self.prompt = self.prompt.strip()[0:-1] + '#'
-            elif self.session: self.prompt = self.prompt.strip()[0:-1] + b'#'
+            if self.shell:
+                self.prompt = "".join(self.prompt.strip()[0:-1], '#')
+            elif self.session:
+                self.prompt = "".join(self.prompt.strip()[0:-1], b'#')
         return self.os
 
-    #Returns digit signifying type of OS
-    def getos(self):
+
+    def get_os(self):
+        """
+        Returns digit signifying type of OS
+        """
         return self.os
 
-    def clearos(self):
+
+    def clear_os(self):
         self.os = 0
 
-    #Takes a list of commands as arg, sets commands to that list
-    def setcommands(self, commands):
+
+    def set_commands(self, commands):
+        """
+        Takes a list of commands as arg, set_s commands to that list
+        """
         self.commands = commands
 
-    #Takes a file with list of commands as arg, sets commands to that list
-    def setcommandsfromfile(self, file):
+
+    def set_commandsfromfile(self, file):
+        """
+        Takes a file with list of commands as arg, set_s commands to that list
+        """
         self.commands = list(open(file, 'r'))
 
-    #Takes a single command as arg, appends command to list of commands
+
     def addcommand(self, command):
+        """
+        Takes a single command as arg, appends command to list of commands
+        """
         self.commands.append(command)
 
-    #Returns list of commands to run
-    def getcommands(self):
+
+    def get_commands(self):
+        """
+        Returns list of commands to run_
+        """
         return self.commands
 
-    def clearcommands(self):
+
+    def clear_commands(self):
         self.commands = []
 
-    #Sets the output of commands run, overwriting any previous entries
-    def setoutput(self, output):
+
+    def set_output(self, output):
+        """
+        Sets the output of commands run, overwriting any previous entries
+        """
         self.output = output
 
-    #Adds to output of commands run
+
     def addtooutput(self, output):
+        """
+        Adds to output of commands run
+        """
         self.output.append(output)
 
-    #Returns the output
-    def getoutput(self):
+
+    def get_output(self):
+        """
+        Returns output from client
+        """
         return self.output
 
-    def clearoutput(self):
+
+    def clear_output(self):
         self.output = []
+
 
     def login(self, **kwargs):
         if kwargs:
             self.host = kwargs.get('host', None)
             self.username = kwargs.get('username', None)
             self.password = kwargs.get('password', None)
-        if not self.verifyloginparameters(): return
-        if self.securelogin() or self.unsecurelogin(): return self.os
+        if not self.verifyloginparameters():
+            return
+        if self.secure_login() or self.unsecurelogin():
+            return self.os
+
 
     async def async_login(self, **kwargs):
         if kwargs:
             self.host = kwargs.get('host', None)
             self.username = kwargs.get('username', None)
             self.password = kwargs.get('password', None)
-        if not self.verifyloginparameters(): return
+        if not self.verifyloginparameters():
+            return
         try:
-            if await self.async_securelogin() or self.unsecurelogin():
+            if await self.async_secure_login() or self.unsecurelogin():
                 return self.os
         except:
             return None
 
-    #Attempts to login to devices via SSH, returns OS type if successful, if not returns 0
-    def securelogin(self, **kwargs):
+
+    def secure_login(self, **kwargs):
+        """
+        Attempts to login to devices via SSH, returns OS type if successful, if not returns 0
+        """
         if kwargs:
             self.host = kwargs.get('host', None)
             self.username = kwargs.get('username', None)
             self.password = kwargs.get('password', None)
-        if not self.verifyloginparameters(): return
+        if not self.verifyloginparameters():
+            return
         try:
-            self.session = paramiko.SSHClient() # Create instance of SSHClient object
-            self.session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.session = paramiko.SSHClient() #Create instance of SSHClient object
+            self.session.set__missing_host_key_policy(paramiko.AutoAddPolicy())
             self.session.connect(self.host.strip('\n'), username=self.username, password=self.password, look_for_keys=False, allow_agent=False)
             self.shell = self.session.invoke_shell()
             time.sleep(3)  #Allow time to log in and strip MOTD
             self.prompt = self.shell.recv(1000).decode().split('\n')[-1].strip()
-            self.setos(self.prompt)
+            self.set_os(self.prompt)
             return self.os
-        except: return
+        except:
+            return
 
-    async def async_securelogin(self, **kwargs):
+
+    async def async_secure_login(self, **kwargs):
         if kwargs:
             self.host = kwargs.get('host', None)
             self.username = kwargs.get('username', None)
             self.password = kwargs.get('password', None)
-        if not self.verifyloginparameters(): return
+        if not self.verifyloginparameters():
+            return
         try:
-            self.session = paramiko.SSHClient() # Create instance of SSHClient object
-            self.session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.session = paramiko.SSHClient() #Create instance of SSHClient object
+            self.session.set__missing_host_key_policy(paramiko.AutoAddPolicy())
             self.session.connect(self.host.strip('\n'), username=self.username, password=self.password, look_for_keys=False, allow_agent=False)
             self.shell = self.session.invoke_shell()
             await asyncio.sleep(3)  #Allow time to log in and strip MOTD
             self.prompt = self.shell.recv(1000).decode().split('\n')[-1].strip()
-            self.setos(self.prompt)
+            self.set_os(self.prompt)
             return self.os
-        except: return
+        except:
+            return
 
-    #Attempts fo login to devices via Telnet, returns OS type if successful, if not returns 0
-    def unsecurelogin(self, **kwargs):
+
+    def unsecure_login(self, **kwargs):
+        """
+        Attempts fo login to devices via Telnet, returns OS type if successful, if not returns 0
+        """
         if kwargs:
             self.host = kwargs.get('host', None)
             self.username = kwargs.get('username', None)
             self.password = kwargs.get('password', None)
-        if not self.verifyloginparameters(): return
-        if type(self.password) != bytes: self.password = self.password.encode()
+        if not self.verifyloginparameters():
+            return
+        if type(self.password) != bytes:
+            self.password = self.password.encode()
         try:
             loginregex = re.compile(b"|".join([b'[Uu]sername', b'[Ll]ogin']))
             promptregex = re.compile(b"|".join([b'[AB]:.*#', b'CPU.*#', b'.*#', b'@.*>']))
@@ -192,17 +302,21 @@ class iTTY:
             self.session.write(self.password + b'\r')
             software, match, previous_text = self.session.expect([promptregex,], 7)
             self.prompt = previous_text.split(b'\n')[-1].strip().decode()
-            self.setos(self.prompt)
+            self.set_os(self.prompt)
             return self.os
-        except: return
+        except:
+            return
 
-    async def async_unsecurelogin(self, **kwargs):
+
+    async def async_unsecure_login(self, **kwargs):
         if kwargs:
             self.host = kwargs.get('host', None)
             self.username = kwargs.get('username', None)
             self.password = kwargs.get('password', None)
-        if not self.verifyloginparameters(): return
-        if type(self.password) != bytes: self.password = self.password.encode()
+        if not self.verifyloginparameters():
+            return
+        if type(self.password) != bytes:
+            self.password = self.password.encode()
         try:
             loginregex = re.compile(b"|".join([b'[Uu]sername', b'[Ll]ogin']))
             promptregex = re.compile(b"|".join([b'[AB]:.*#', b'CPU.*#', b'.*#', b'@.*>']))
@@ -213,41 +327,57 @@ class iTTY:
             self.session.write(self.password + b'\r')
             software, match, previous_text = self.session.expect([promptregex,], 7)
             self.prompt = previous_text.split(b'\n')[-1].strip().decode()
-            self.setos(self.prompt)
+            self.set_os(self.prompt)
             return self.os
-        except: return
+        except:
+            return
 
-    def runcommands(self, command_delay, commandheader=0, done=False):
-        if self.shell: return self.runseccommands(command_delay, commandheader=commandheader, done=done)
-        elif self.session: return self.rununseccommands(command_delay, commandheader=commandheader, done=done)
 
-    async def async_runcommands(self, command_delay, commandheader=0, done=False):
-        if self.shell: return await self.async_runseccommands(command_delay, commandheader=commandheader, done=done)
-        elif self.session: return await self.async_rununseccommands(command_delay, commandheader=commandheader, done=done)
+    def run_commands(self, command_delay, commandheader=0, done=False):
+        if self.shell:
+            return self.run_sec_commands(command_delay, commandheader=commandheader, done=done)
+        elif self.session:
+            return self.run_unsec_commands(command_delay, commandheader=commandheader, done=done)
 
-    #Runs commands when logged in via SSH, returns output
-    def runseccommands(self, command_delay, commandheader=0, done=False):
-        for command in self.getcommands():
+
+    async def async_run_commands(self, command_delay, commandheader=0, done=False):
+        if self.shell:
+            return await self.async_run_sec_commands(command_delay, commandheader=commandheader, done=done)
+        elif self.session:
+            return await self.async_run_unsec_commands(command_delay, commandheader=commandheader, done=done)
+
+
+    def run_sec_commands(self, command_delay, commandheader=0, done=False):
+        """
+        Runs commands when logged in via SSH, returns output
+        """
+        for command in self.get_commands():
             self.shell.send(command.strip() + '\r')
             time.sleep(command_delay)
             if commandheader:
                 self.addtooutput(['\n' + _underline(command), ])
             self.addtooutput(self.shell.recv(500000).decode().split('\n')[1:])
-        if done: self.logout()
-        return self.getoutput()
+        if done:
+            self.logout()
+        return self.get_output()
 
-    async def async_runseccommands(self, command_delay, commandheader=0, done=False):
-        for command in self.getcommands():
+
+    async def async_run_sec_commands(self, command_delay, commandheader=0, done=False):
+        for command in self.get_commands():
             self.shell.send(command.strip() + '\r')
             await asyncio.sleep(command_delay)
             if commandheader:
                 self.addtooutput(['\n' + _underline(command), ])
             self.addtooutput(self.shell.recv(500000).decode().split('\n')[1:])
-        if done: self.logout()
-        return self.getoutput()
+        if done:
+            self.logout()
+        return self.get_output()
 
-    #Runs commands when logged in via Telnet, returns output
-    def rununseccommands(self, command_delay, commandheader=0, done=False):
+
+    def run_unsec_commands(self, command_delay, commandheader=0, done=False):
+        """
+        Runs commands when logged in via Telnet, returns output
+        """
         for command in self.commands:
             self.session.write((command.strip() + '\r').encode())
             n, m, output = self.session.expect([re.compile(self.prompt.encode()), ], command_delay)
@@ -255,10 +385,12 @@ class iTTY:
             if commandheader:
                 self.addtooutput(['\n' + _underline(command), ])
             self.addtooutput(output.decode().split('\n')[1:])
-        if done: self.logout()
-        return self.getoutput()
+        if done:
+            self.logout()
+        return self.get_output()
 
-    async def async_rununseccommands(self, command_delay, commandheader=0, done=False):
+
+    async def async_run_unsec_commands(self, command_delay, commandheader=0, done=False):
         for command in self.commands:
             self.session.write((command.strip() + '\r').encode())
             n, m, output = self.session.expect([re.compile(self.prompt.encode()), ], command_delay)
@@ -266,13 +398,18 @@ class iTTY:
             if commandheader:
                 self.addtooutput(['\n' + _underline(command), ])
             self.addtooutput(output.decode().split('\n')[1:])
-        if done: self.logout()
-        return self.getoutput()
+        if done:
+            self.logout()
+        return self.get_output()
+
 
     def logout(self):
-        if self.shell: self.shell.close()
-        elif self.session: self.session.close()
+        if self.shell:
+            self.shell.close()
+        elif self.session:
+            self.session.close()
         return
+
 
     def siftoutput(self, *siftout):
         dontprint = ['enable', 'Password:', 'terminal length', 'screen-length', 'Screen length', \
@@ -281,12 +418,15 @@ class iTTY:
         output= []
         for entry in self.output:
             for line in entry:
-                if not line.strip() or any(str(n) in line for n in dontprint): continue
+                if not line.strip() or any(str(n) in line for n in dontprint):
+                    continue
                 output.append(line)
         return output
 
+
 def _underline(input, linechar="-"):
     return input.strip() + '\n' + _makeline(len(input.strip()), linechar)
+
 
 def _makeline(count, linechar="-"):
     return linechar * int(count)
