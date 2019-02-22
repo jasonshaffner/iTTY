4c15b55c06dfddf619c55e4418f2b094d8829acc
Jason Shaffner <jason.shaffner@charter.com>
2019-02-22 15:27:12 -0700

    updated custom exceptions to propagate more data to user

--- iTTY/__init__.py
+++ iTTY/__init__.py
@@ -1,6 +1,7 @@
 from . import Extractor
 from .iTTY import iTTY
 from .iTTY import CouldNotConnectError
+from .iTTY import BrokenConnectionError
 from .iTTY import LoginParametersNotSpecifiedError
 
-__all__ = ['iTTY', 'Extractor', 'CouldNotConnectError', 'LoginParamatersNotSpecifiedError']
+__all__ = ['iTTY', 'Extractor', 'CouldNotConnectError', 'BrokenConnectionError', 'LoginParamatersNotSpecifiedError']
