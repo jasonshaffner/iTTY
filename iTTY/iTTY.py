0d1e0505100747fca4932087fb04da759e592993
Jason Shaffner <jason.shaffner@charter.com>
2019-04-12 10:29:00 -0600

    added EOF error to aggregation into BrokenConnectionError

--- iTTY/iTTY.py
+++ iTTY/iTTY.py
@@ -683,7 +683,7 @@ class iTTY:
             self.session.read_very_eager()
             self.session.write(command.strip() + b'\r')
             return await self._async_expect(command_delay=command_delay)
-        except (BrokenPipeError, ConnectionResetError) as e:
+        except (BrokenPipeError, ConnectionResetError, EOFError) as e:
             raise BrokenConnectionError(self.host, e)
 
 
@@ -704,7 +704,7 @@ class iTTY:
             if not match:
                 self._async_run_unsec_command('q', 1)
             return ansi_escape.sub('', output.decode(errors='ignore'))
-        except (EOFError, ConnectionResetError) as e:
+        except (BrokenPipeError, EOFError, ConnectionResetError) as e:
             raise BrokenConnectionError(self.host, e)
 
     def logout(self):
