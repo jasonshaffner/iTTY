5b253b1867c245c5d85a8329e6db0a64c6792867
Jason Shaffner <jason.shaffner@charter.com>
2019-04-18 17:08:57 -0600

    update readme

--- setup.py
+++ setup.py
@@ -7,25 +7,26 @@ setup(
     author='Jason Shaffner',
     packages=['iTTY', 'iTTY.utils'],
     license="LGPL",
-	scripts=[
-		'scripts/run_commands',
-		'scripts/check_version',
-                'scripts/check_contact',
-		'scripts/check_hostname',
-		'scripts/check_location',
-		'scripts/check_syslog',
-		'scripts/check_trap_collectors',
-                'scripts/verify_login',
-                'scripts/can_i_login',
-                'scripts/can_i_run_commands',
-        ],
-        classifiers=[
-            "Development Status :: 4 - Beta",
-            "Intended Audience :: Developers",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 3.7",
-        ],
-        requires=[
-            'paramiko',
-        ]
+    scripts=[
+            'scripts/run_commands',
+            'scripts/check_version',
+            'scripts/check_contact',
+            'scripts/check_hostname',
+            'scripts/check_location',
+            'scripts/check_syslog',
+            'scripts/check_trap_collectors',
+            'scripts/check_v4_interfaces',
+            'scripts/verify_login',
+            'scripts/can_i_login',
+            'scripts/can_i_run_commands',
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+    ],
+    requires=[
+        'paramiko',
+    ]
 )
